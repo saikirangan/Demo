6062e040465eda594b79703a15f32f74717ccf7d
saikirangan <135574001+saikirangan@users.noreply.github.com>
2023-09-13 17:07:01 +0530

    Create hello.cbl

---
+++ hello.cbl
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.                                         
+       PROGRAM-ID. PGM01.                                               
+       ENVIRONMENT DIVISION.                                           
+       DATA DIVISION.                                                   
+       PROCEDURE DIVISION.                                           
+           DISPLAY 'HELLO WORLD'.                                       
+           DISPLAY 'THIS IS POPUP'.
+           STOP RUN.                                                    
